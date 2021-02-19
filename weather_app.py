3babf602a0f3c1f0e5c26998ea8517c2e3d0148e
IgorTelepenko <telepenko.igor2@ukr.net>
2021-02-19 09:24:57 +0200

    adding quick search buttons + small refactoring

--- weather_app.py
+++ weather_app.py
@@ -32,6 +32,17 @@ def getWeather(city):
     else:
         return None
 
+def search() :
+    city = cityText.get()
+    weather = getWeather(city)
+    if weather:
+        locationLbl['text'] = '{},{}'.format(weather[0],weather[1])
+        img['file'] = 'weather_icons/{}.png'.format(weather[3])
+        temperLbl['text'] = '{:.2f}°C'.format(weather[2])
+        weatherLbl['text'] = weather[4]
+    else:
+        messagebox.showerror('Error', 'Cannot find {}'.format(city))
+
 def getWeatherDetails():
     city = cityText.get()
     res = requests.get(urlAPI.format(city, apiKey))
@@ -53,48 +64,6 @@ def getWeatherDetails():
         return finalRet
     else:
         return None
-
-#print(getWeather('London'))
-
-def search() :
-    city = cityText.get()
-    weather = getWeather(city)
-    if weather:
-        locationLbl['text'] = '{},{}'.format(weather[0],weather[1])
-        img['file'] = 'weather_icons/{}.png'.format(weather[3])
-        temperLbl['text'] = '{:.2f}°C'.format(weather[2])
-        weatherLbl['text'] = weather[4]
-    else:
-        messagebox.showerror('Error', 'Cannot find {}'.format(city))
-
-
-app = Tk()
-app.title("Weather app")
-app.geometry('700x350')
-
-cityText = StringVar()
-cityEntry = Entry(app, textvariable = cityText)
-cityEntry.pack() #placing it on the screen
-
-searchBtn = Button(app, text = 'Search weather', width = 12, command = search)
-searchBtn.pack()
-
-locationLbl = Label(app, text = 'Location', font = ('bold', 20))
-locationLbl.pack()
-
-img = PhotoImage(file = '')
-image = Label(app, image = img)
-image.pack()
-
-temperLbl = Label(app, text = 'Temperature')
-temperLbl.pack()
-
-weatherLbl = Label(app, text = 'Weather')
-weatherLbl.pack()
-
-
-    
-
 
 def openDescr():
     detailsApp = Tk()
@@ -144,8 +113,74 @@ def openDescr():
 
     detailsApp.mainloop()
 
+def quickRun(ind):
+    if ind == 1:
+        cityText.set(quickSearch1Btn['text'])
+    if ind == 2:
+        cityText.set(quickSearch2Btn['text'])
+    if ind == 3:
+        cityText.set(quickSearch3Btn['text'])
+    search()
+
+def writeQuickBut(ind):
+    if cityText.get() != '':
+        if ind == 1:
+            quickSearch1Btn['text'] = cityText.get()
+        if ind == 2:
+            quickSearch2Btn['text'] = cityText.get()
+        if ind == 3:
+            quickSearch3Btn['text'] = cityText.get()
+
+app = Tk()
+app.title("Weather app")
+app.geometry('700x350')
+
+cityText = StringVar()
+cityEntry = Entry(app, textvariable = cityText)
+cityEntry.pack() #placing it on the screen
+
+searchBtn = Button(app, text = 'Search weather', width = 12, command = search)
+searchBtn.pack()
+
+locationLbl = Label(app, text = 'Location', font = ('bold', 20))
+locationLbl.pack()
+
+img = PhotoImage(file = '')
+image = Label(app, image = img)
+image.pack()
+
+temperLbl = Label(app, text = 'Temperature')
+temperLbl.pack()
+
+weatherLbl = Label(app, text = 'Weather')
+weatherLbl.pack()
+
+
 
 detailedDescrBtn = Button(app, text = 'View Details', width = 15, command = openDescr)
 detailedDescrBtn.pack()
+
+
+
+pencilImg = PhotoImage(file = 'tech_icons/pencil.png')
+writeQuick1Btn = Button(app, image = pencilImg, command = lambda: writeQuickBut(1))
+writeQuick1Btn.place(x = 480, y = 200)
+
+writeQuick2Btn = Button(app, image = pencilImg, command = lambda: writeQuickBut(2))
+writeQuick2Btn.place(x = 480, y = 225)
+
+writeQuick3Btn = Button(app, image = pencilImg, command = lambda: writeQuickBut(3))
+writeQuick3Btn.place(x = 480, y = 250)
+
+
+quickSearch1Btn = Button(app, text = 'Casablanca', width = 25, command = lambda: quickRun(1))
+quickSearch1Btn.place(x=500,y=200)
+
+quickSearch2Btn = Button(app, text = 'Vinnytsia', width = 25, command = lambda: quickRun(2))
+quickSearch2Btn.place(x=500,y=225)
+
+quickSearch3Btn = Button(app, text = 'Warsaw', width = 25, command = lambda: quickRun(3))
+quickSearch3Btn.place(x=500,y=250)
+
 
 app.mainloop()
